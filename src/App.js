342f074e41e80f139e0177c8da10cb4b59338f37
Nancy Lu <nancyapple168168@gmail.com>
2023-07-20 01:31:24 +0800

    檔案名稱修改

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
-import Homepage from "./pages/Homepage";
+import Homepage from "./container/Homepage";
 //網址Routing
 //import About from "./pages/About";
 //import { Switch, Route } from "react-router-dom";
