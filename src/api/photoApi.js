dfa834568d3082f976024c48f3c468f3ef66b50c
Nancy Lu <nancyapple168168@gmail.com>
2023-07-21 17:47:58 +0800

    photo fix

---
+++ src/api/photoApi.js
@@ -0,0 +1,13 @@
+import { Photo } from "./api";
+const settings = {
+  headers: {
+    Accept: "application/json",
+    Authorization: "563492ad6f91700001000001ac22187f3df749b5ac359637c75cb9b5",
+  },
+};
+
+export const getPhoto = (num) =>
+  Photo.get(`curated?page=${num}&per_page=15`, settings);
+
+export const getSearchPhoto = (currentSearch, num) =>
+  Photo.get(`search?query=${currentSearch}&per_page=15&page=${num}`, settings);
--- src/container/Homepage.js
+++ src/container/Homepage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Search from "../components/Search";
 import Picture from "../components/Picture";
+import { getPhoto, getSearchPhoto } from "../api/photoApi";
 
 const Homepage = () => {
   const [input, setInput] = useState("");
@@ -8,58 +9,38 @@ const Homepage = () => {
   let [data, setData] = useState(null);
   let [page, setPage] = useState(1);
   const [currentSearch, setCurrentSearch] = useState("");
-  const auth = "563492ad6f91700001000001ac22187f3df749b5ac359637c75cb9b5";
-  //在第一頁的位置有要15張相片
-  const intialURL = "https://api.pexels.com/v1/curated?page=1&per_page=15";
-  const searchURL = `https://api.pexels.com/v1/search?query=${currentSearch}&per_page=15&page=1`;
 
   // 從 pexels api 獲取數據
-  const search = async (url) => {
+  const search = async () => {
+    let dataFetch;
     setPage(2);
-    const dataFetch = await fetch(url, {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-        Authorization: auth,
-      },
-    });
+    if (currentSearch === "") {
+      dataFetch = await getPhoto(1);
+    } else {
+      dataFetch = await getSearchPhoto(currentSearch, 1);
+    }
     //抓來的圖片
-    let parsedData = await dataFetch.json();
+    let parsedData = dataFetch.data.photos;
     //在data顯示15張照片
-    setData(parsedData.photos);
+    setData(parsedData);
   };
 
   // 加載更多圖片
   const morepicture = async () => {
-    let newURL;
+    let dataFetch;
     if (currentSearch === "") {
-      newURL = `https://api.pexels.com/v1/curated?page=${page}&per_page=15`;
+      dataFetch = await getPhoto(1);
     } else {
-      newURL = `https://api.pexels.com/v1/search?query=${currentSearch}&per_page=15&page=${page}`;
+      dataFetch = await getSearchPhoto(currentSearch, 1);
     }
     setPage(page + 1);
-    const dataFetch = await fetch(newURL, {
-      method: "GET",
-      headers: {
-        Accept: "application/json",
-        Authorization: auth,
-      },
-    });
-    let parsedData = await dataFetch.json();
-    setData(data.concat(parsedData.photos));
+    let parsedData = dataFetch.data.photos;
+    setData(data.concat(parsedData));
   };
 
   // 頁面加載時獲取數據
   useEffect(() => {
-    search(intialURL);
-  }, []);
-  //開啟頁面時直接跑圖片
-  useEffect(() => {
-    if (currentSearch === "") {
-      search(intialURL);
-    } else {
-      search(searchURL);
-    }
+    search();
   }, [currentSearch]);
 
   return (
